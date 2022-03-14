706d5bf36979449e05ce9d1c7b5a8bfc6754ce8b
sky6min <sky6min@gmail.com>
2022-03-14 17:00:22 +0900

    feat: add SpringBoot init

---
+++ backend/backend-java/src/main/java/com/ssafy/oauth/entity/ProviderType.java
@@ -0,0 +1,12 @@
+package com.ssafy.oauth.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum ProviderType {
+    GOOGLE,
+    FACEBOOK,
+    NAVER,
+    KAKAO,
+    LOCAL;
+}
---
+++ backend/backend-java/src/main/java/com/ssafy/oauth/info/OAuth2UserInfoFactory.java
@@ -0,0 +1,23 @@
+package com.ssafy.oauth.info;
+
+
+
+import com.ssafy.oauth.entity.ProviderType;
+import com.ssafy.oauth.info.impl.FacebookOAuth2UserInfo;
+import com.ssafy.oauth.info.impl.GoogleOAuth2UserInfo;
+import com.ssafy.oauth.info.impl.KakaoOAuth2UserInfo;
+import com.ssafy.oauth.info.impl.NaverOAuth2UserInfo;
+
+import java.util.Map;
+
+public class OAuth2UserInfoFactory {
+    public static OAuth2UserInfo getOAuth2UserInfo(ProviderType providerType, Map<String, Object> attributes) {
+        switch (providerType) {
+            case GOOGLE: return new GoogleOAuth2UserInfo(attributes);
+            case FACEBOOK: return new FacebookOAuth2UserInfo(attributes);
+            case NAVER: return new NaverOAuth2UserInfo(attributes);
+            case KAKAO: return new KakaoOAuth2UserInfo(attributes);
+            default: throw new IllegalArgumentException("Invalid Provider Type.");
+        }
+    }
+}
