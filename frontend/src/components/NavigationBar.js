cbaf21082abbdefa3b594d6794ca0fa470311eaa
이혜진 <hy2jinl22@gmail.com>
2022-03-17 03:29:33 +0900

    feat : add NavigationBar.js

---
+++ frontend/src/components/NavigationBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import './scss/NavigationBar.scss'
+
+const NavigationBar = ({boldPath}) => {
+  let history = useHistory()
+  const onClickLogo = () => {
+    history.push('/')
+  }
+
+  return (
+    <nav className='navbar'>
+      <img src={`${process.env.PUBLIC_URL}/logo.png`} className='ssafit-logo' onClick={onClickLogo} alt='ssafit-logo' />
+      <ul className='menu'>
+        <li><Link className='link' to="#" id={boldPath === "TPO" ? "active" : ""}>TPO</Link></li>
+        <li><Link className='link' to="#" id={boldPath === "RECOMMEND" ? "active" : ""}>RECOMMEND</Link></li>
+        <li><Link className='link' to="#" id={boldPath === "MYPAGE" ? "active" : ""}>MYPAGE</Link></li>
+        <li><Link className='link' to='#'>SEARCH</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavigationBar;
