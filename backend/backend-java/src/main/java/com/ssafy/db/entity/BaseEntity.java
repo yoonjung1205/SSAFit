f8a713e6ddcc3440abd9282ef5eaf52e9092ce1f
sky6min <sky6min@gmail.com>
2022-03-18 01:03:33 +0900

    feat: add backend
    Former-commit-id: 469c28ed6c202139f3d21c5b1a8ed91bb8a053fd

---
+++ backend/backend-java/src/main/java/com/ssafy/db/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.ssafy.db.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * 모델 간 공통 사항 정의.
+ */
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(updatable = false)
+    private LocalDateTime updatedAt;
+}
