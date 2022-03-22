767dbf5a843ac20176d5790d1766521dc37cf77e
LKH <mogies@naver.com>
2022-03-22 16:12:23 +0900

    feat: add signup.js
    Former-commit-id: 49580c6d15d32e3c3338844ef9914a4aa763c328

--- frontend/src/pages/signup/Signup.js
+++ frontend/src/pages/signup/Signup.js
@@ -6,28 +6,36 @@ import './scss/signup.scss'
 
 
 
-export default function Signup() {
+export default function Signup({ history }) {
   const [credentials, setCredentials] = useState({
     email: null, password: null, passwordConf: null
   })
   const [validData, setValidData] = useState({
     email: null, password: null, passwordConf: null
   })
+  const [hover, setHover] = useState({
+    email: false, password: false, passwordConf: false
+  })
   
 
   const signup = function(event){
     event.preventDefault()
+    ////////////// 회원가입  /////////////////
+    history.push('/moreinfo')
   }
 
 
   const validator = function(target){
     if (target === 'email'){
-      
+      /////////////// 이메일 중복검사 //////////////////
     }
     else if (target ==='password'){
-      const passValidator = new RegExp('[0-9a-zA-Z~!@#$%^&*()_+-=[]{};\':",\\|./<>?]{8,16}')
+      const passValidator = /[0-9a-zA-Z~!@#$%^&*()_+-=[\]{};\':",\\|.\/<>?]{8,16}/
       const result = passValidator.exec(credentials.password)
-      if (result[0] === credentials.password){
+      if (!credentials.password){
+        setValidData({...validData, password: null})
+      }
+      else if (result && result[0] === credentials.password){
         setValidData({...validData, password: 1})
       }
       else {
@@ -35,11 +43,16 @@ export default function Signup() {
       }
     }
     else if (target === 'passwordConf'){
-      if (credentials.password === credentials.passwordConf){
-        return true
+      if (!credentials.passwordConf){
+        setValidData({...validData, passwordConf: null})
+      }
+      else if (credentials.password === credentials.passwordConf){
+        setValidData({...validData, passwordConf: 1})
+      }
+      else {
+        setValidData({...validData, passwordConf: -1})
       }
     }
-    return false
   }
 
 
@@ -53,34 +66,86 @@ export default function Signup() {
       <section className='signup-body'>
         <h1>Sign Up</h1>
         <form onSubmit={event => signup(event)}>
+          {/* 이메일 */}
           <label>
             이메일
             <div id='email-input'>
-              <input type="email" name="email" id="email"
-              placeholder='이메일을 입력하세요'
-              onInput={event => {credentials.email = event.target.value}} />
+              <div className='input-box'>
+                <input type="email" name="email" id="email"
+                placeholder='이메일을 입력하세요'
+                onInput={event => {credentials.email = event.target.value}} />
+
+                <img className='validator-helper' src={validData.email === 1 ? corr:incorr}
+                  style={{display: validData.email ? 'block':'none'}} alt="helper"
+                  onMouseOver={() => setHover({...hover, email: true})}/>
+
+                <div className={`helper-message ${validData.email === 1 ? 'corr':'incorr'}`} 
+                  style={{display: hover.email === true ? 'block':'hidden'}}>
+                  <div className={`arrow ${validData.email === 1 ? 'arrow-corr':'arrow-incorr'}`} />
+                  <p className="helper-message-corr" style={{display: validData.email === 1 ? 'block':'none'}}>
+                    사용 가능한 이메일입니다!
+                  </p>
+                  <p className='helper-message-incorr' style={{display: validData.email === -1 ? 'block':'none'}}>
+                    사용중인 이메일입니다.
+                  </p>
+                </div>
+              </div>
               <button className='eamil-validator' onClick={() => {validator('email')}} >
                 <span />
                 <p>확인</p>
               </button>
             </div>
-            <img className='validator-helper' src={(validData.email && validator.email === 1) ? corr:incorr} alt="correct"/>
           </label>
+          {/* 비밀번호 */}
           <label>
             비밀번호
-            <input type="password" name="password" id="password"
-             placeholder='비밀번호는 8 ~ 16글자, 특수문자, 영어, 숫자 1개 이상 포함해야 합니다'
-             onInput={event => setCredentials({...credentials, password: event.target.value})}
-             onBlur={() => {validator('password')}} />
-            <img className='validator-helper' src={(validData.password && validator.password === 1) ? corr:incorr} alt="correct"/>
+            <div className='input-box'>
+              <input type="password" name="password" id="password"
+              placeholder='비밀번호는 8 ~ 16글자, 특수문자, 영어, 숫자 1개 이상 포함해야 합니다'
+              onInput={event => setCredentials({...credentials, password: event.target.value})}
+              onBlur={() => {validator('password')}} />
+
+              <img className='validator-helper' src={validData.password === 1 ? corr:incorr}
+                style={{display: validData.password ? 'block':'none'}} alt="helper"
+                onMouseOver={() => setHover({...hover, password: true})}
+                onMouseOut={() => setHover({...hover, password: false})}/>
+
+              <div className={`helper-message ${validData.password === 1 ? 'corr':'incorr'}`} 
+                style={{display: hover.password === true ? 'block':'none'}}>
+                <div className={`arrow ${validData.password === 1 ? 'arrow-corr':'arrow-incorr'}`} />
+                <p className="helper-message-corr" style={{display: validData.password === 1 ? 'block':'none'}}>
+                  올바른 비밀번호 입니다!
+                </p>
+                <p className='helper-message-incorr' style={{display: validData.password === -1 ? 'block':'none'}}>
+                  올바르지 않은 비밀번호입니다.
+                </p>
+              </div>
+            </div>
           </label>
           <label>
             비밀번호 확인
-            <input type="password" name="password-conf" id="password-conf"
-             placeholder='비밀번호를 다시 입력하세요'
-             onInput={event => setCredentials({...credentials, passwordConf: event.target.value})}
-             onBlur={() => {validator('passwordConf')}} />
-            <img className='validator-helper' src={(validData.passwordConf && validator.passwordConf === 1) ? corr:incorr} alt="correct"/>
+            <div className='input-box'>
+              <input type="password" name="password-conf" id="password-conf"
+              placeholder='비밀번호를 다시 입력하세요'
+              onInput={event => setCredentials({...credentials, passwordConf: event.target.value})}
+              onBlur={() => {validator('passwordConf')}} />
+
+              <img className='validator-helper' src={validData.passwordConf === 1 ? corr:incorr}
+                style={{display: validData.passwordConf ? 'block':'none'}} alt="helper"
+                onMouseOver={() => setHover({...hover, passwordConf: true})}
+                onMouseOut={() => setHover({...hover, passwordConf: false})}/>
+
+              <div className={`helper-message ${validData.passwordConf === 1 ? 'corr':'incorr'}`} 
+                style={{display: hover.passwordConf === true ? 'block':'none'}}>
+                <div className={`arrow ${validData.passwordConf === 1 ? 'arrow-corr':'arrow-incorr'}`} />
+                <p className="helper-message-corr" style={{display: validData.passwordConf === 1 ? 'block':'none'}}>
+                  비밀번호가 일치합니다!
+                </p>
+                <p className='helper-message-incorr' style={{display: validData.passwordConf === -1 ? 'block':'none'}}>
+                  비밀번호가 맞지 않습니다.
+                </p>
+              </div>
+            </div>
           </label>
           <button>
             <span/>
