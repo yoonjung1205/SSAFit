77f4fa0a172e40c43132e632d0735808449c162f
이혜진 <hy2jinl22@gmail.com>
2022-03-23 00:15:58 +0900

    feat: add Recommend
    Former-commit-id: 786f1e2ca20e2c4455880bba6a56762ba096d68c

---
+++ frontend/src/pages/recommend/compoenets/category.js
@@ -0,0 +1,77 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import '../scss/category.scss'
+
+const category = ({cate}) => {
+  return (
+    <div className='rec-category'>
+      <div className='rec-cate-text'>
+        <h3>{cate}</h3>
+        <p>더보기</p>
+      </div>
+      <div className='rec-cate-cloth'>
+        <div className='card'>
+          <div className='card-image'>
+            <img src='//image.msscdn.net/images/goods_img/20180824/836981/836981_2_500.jpg' alt='cloth-image' />
+          </div>
+          <div className='card-text'>
+            <p>브랜드는 한줄</p>
+            <p>옷 이름은 두줄</p>
+            <p>가격은 한줄</p>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='card-image'>
+            <img src='//image.msscdn.net/images/goods_img/20180824/836981/836981_2_500.jpg' alt='cloth-image' />
+          </div>
+          <div className='card-text'>
+            <p>브랜드는 한줄</p>
+            <p>옷 이름은 두줄</p>
+            <p>가격은 한줄</p>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='card-image'>
+            <img src='//image.msscdn.net/images/goods_img/20180824/836981/836981_2_500.jpg' alt='cloth-image' />
+          </div>
+          <div className='card-text'>
+            <p>브랜드는 한줄</p>
+            <p>옷 이름은 두줄</p>
+            <p>가격은 한줄</p>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='card-image'>
+            <img src='//image.msscdn.net/images/goods_img/20180824/836981/836981_2_500.jpg' alt='cloth-image' />
+          </div>
+          <div className='card-text'>
+            <p>브랜드는 한줄</p>
+            <p>옷 이름은 두줄</p>
+            <p>가격은 한줄</p>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='card-image'>
+            <img src='//image.msscdn.net/images/goods_img/20180824/836981/836981_2_500.jpg' alt='cloth-image' />
+          </div>
+          <div className='card-text'>
+            <p>브랜드는 한줄</p>
+            <p>옷 이름은 두줄</p>
+            <p>가격은 한줄</p>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='card-image'>
+            <img src='//image.msscdn.net/images/goods_img/20180824/836981/836981_2_500.jpg' alt='cloth-image' />
+          </div>
+          <div className='card-text'>
+            <p>브랜드는 한줄</p>
+            <p>옷 이름은 두줄</p>
+            <p>가격은 한줄</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default category;
