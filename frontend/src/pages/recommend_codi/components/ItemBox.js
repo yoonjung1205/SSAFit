c7967102de79c3e2250815ee0724ff699413a42b
LKH <mogies@naver.com>
2022-03-24 23:21:44 +0900

    feat: add RecommendCodi.js
    Former-commit-id: 049af65d9270536d0fbabee657bde419eef36949

--- frontend/src/pages/recommend_codi/components/ItemBox.js
+++ frontend/src/pages/recommend_codi/components/ItemBox.js
@@ -1,9 +1,39 @@
+import axios from 'axios'
 import React from 'react'
+import heart from '../images/heart.png'
 
-export default function itembox({ items }) {
+export default function itembox({ items, setItems }) {
+  // const baseUrl = 'https://ssafit.site/api_be'
+  // const like = function(){
+  //   axios({
+  //     method: 'post',
+  //     url: '',
+  //     body: {}
+  //   })
+  // }
+
+  if (items.lenght === 0){
+    return <>Loding...</>
+  }
+
+  const cards = items.map((item, idx) => (
+    <div className='item-card' key={idx}>
+      <img className='item-img' src={item.imageUrl} alt={item.name} />
+      <div className='item-content'>
+        <p>{item.brand}</p>
+        <p>{item.name}</p>
+        <p>{item.price}</p>
+        <span className={item.liked ? 'liked':''}
+          onClick={() => setItems([...items], items[idx].liked = !items[idx].liked)}>
+          <img src={heart} alt="찜하기" />
+        </span>
+      </div>
+    </div>
+  ))
+
   return (
     <div className='item-box'>
-      {items}
+      {cards}
     </div>
   )
 }
