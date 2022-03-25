1a1285f26b2b902badb60f0a1f3728f96f2ad9a4
sky6min <sky6min@gmail.com>
2022-03-25 11:21:32 +0900

    feat : update userController

--- backend/backend-java/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend/backend-java/src/main/java/com/ssafy/api/controller/UserController.java
@@ -73,7 +73,7 @@ public class UserController {
         @ApiResponse(code = 500, message = "서버 오류")
     })
 	public ResponseEntity<? extends BaseResponseBody> register(
-			@RequestBody @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo, HttpServletResponse response, MultipartHttpServletRequest request) {
+			@RequestBody @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo, HttpServletResponse response) {
 
 
 		//임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
