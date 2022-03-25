daf5cb3eccabdacfae60368d9b008fe2c7b569e1
sky6min <sky6min@gmail.com>
2022-03-25 10:56:37 +0900

    feat : update signup

--- backend/backend-java/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend/backend-java/src/main/java/com/ssafy/api/controller/UserController.java
@@ -32,6 +32,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletResponse;
@@ -72,18 +73,15 @@ public class UserController {
         @ApiResponse(code = 500, message = "서버 오류")
     })
 	public ResponseEntity<? extends BaseResponseBody> register(
-			@RequestBody @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo, HttpServletResponse response) {
+			@RequestBody @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo, HttpServletResponse response, MultipartHttpServletRequest request) {
 
 
 		//임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
 		User u = userService.createUser(registerInfo);
 
 		String userEmail = registerInfo.getEmail();
-		String password = registerInfo.getPassword();
-		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(10);
+
 		User user = userService.getUserByEmail(userEmail);
-		// 로그인 요청한 유저로부터 입력된 패스워드와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
-		if(bCryptPasswordEncoder.matches(password, user.getPassword())) {
 
 			UserRefreshToken userRefreshToken = userRefreshTokenRepository.findByUserId(userEmail);
 
@@ -104,9 +102,7 @@ public class UserController {
 			response.setHeader("authorization",accessToken);
 			// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
 			return ResponseEntity.ok(UserLoginPostRes.ofs(200, "Success"));
-		}
-		// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
-		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
+
 
 	}
 
