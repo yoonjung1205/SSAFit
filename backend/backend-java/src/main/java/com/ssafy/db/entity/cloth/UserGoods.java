0a4c55a687409964c16f41d7a820b61f710209c9
sky6min <sky6min@gmail.com>
2022-03-25 10:04:26 +0900

    feat : add JPA  repository

---
+++ backend/backend-java/src/main/java/com/ssafy/db/entity/cloth/UserGoods.java
@@ -0,0 +1,22 @@
+package com.ssafy.db.entity.cloth;
+
+import com.ssafy.db.entity.User;
+import lombok.Data;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Data
+@Embeddable
+public class UserGoods implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name="USER_ID")
+    User user;
+
+    @ManyToOne
+    @JoinColumn(name="GOODS_ID")
+    Goods goods;
+}
---
+++ backend/backend-java/src/main/java/com/ssafy/db/entity/codi/UserCodi.java
@@ -0,0 +1,23 @@
+package com.ssafy.db.entity.codi;
+
+import com.ssafy.db.entity.User;
+import com.ssafy.db.entity.cloth.Goods;
+import lombok.Data;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Data
+@Embeddable
+public class UserCodi implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name="USER_ID")
+    User user;
+
+    @ManyToOne
+    @JoinColumn(name="CODI_ID")
+    Codi codi;
+}
