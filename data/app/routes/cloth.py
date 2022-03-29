8536383f51bc3b79265be3c51ff1ad9fe5056dc6
yoonjung1205 <yoonjung1205@naver.com>
2022-03-29 14:22:55 +0900

    feat: update the way of recommend codi(pick randomly by size 20)

--- data/app/routes/cloth.py
+++ data/app/routes/cloth.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 from ..database.database import *
 from fastapi.responses import JSONResponse
-import json
-import time
 
 
 router = APIRouter()
@@ -15,9 +13,6 @@ async def getCloth(newClothId: int):
 
 @router.get('/codi/{codiTPO}', tags=["Codi"])
 async def getCodi(codiTPO: str):
-    start = time.time()
     result = await get_codi(codiTPO)
-    finish = time.time()
-    print(finish-start)
     # return result[:4] # 스웨거 확인용
     return result
