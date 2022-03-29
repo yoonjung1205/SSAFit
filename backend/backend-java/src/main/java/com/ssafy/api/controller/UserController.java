56a0b78b303311b5b2931fc900224c5985accb46
sky6min <sky6min@gmail.com>
2022-03-29 13:48:53 +0900

    feat : update oauth google

--- backend/backend-java/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend/backend-java/src/main/java/com/ssafy/api/controller/UserController.java
@@ -95,7 +95,7 @@ public class UserController {
 		System.out.println("fileUrl : " + fileUrl);
 
 		// oauth 로그인 시.
-		if(registerInfo.getPassword().length() == 0) {
+		if(registerInfo.getPassword() == null) {
 			//임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
 			User u = userService.OAuthcreateUser(registerInfo,fileUrl);
 
