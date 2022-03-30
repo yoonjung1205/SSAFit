14a2157249c18e2938d60e99e050d26511e5b17f
sky6min <sky6min@gmail.com>
2022-03-30 14:56:06 +0900

    feat : add searchCloth by name

---
+++ backend/backend-java/src/main/java/com/ssafy/api/response/GoodsListRes.java
@@ -0,0 +1,20 @@
+package com.ssafy.api.response;
+
+import com.ssafy.mongodb.entity.Cloth;
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ApiModel("GoodsListRes")
+@AllArgsConstructor
+@NoArgsConstructor
+public class GoodsListRes {
+
+    private List<Cloth> goodsList;
+}
---
+++ backend/backend-java/src/main/java/com/ssafy/api/service/GoodsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.ssafy.api.service;
+
+import com.ssafy.api.response.GoodsListRes;
+import com.ssafy.mongodb.entity.Cloth;
+import com.ssafy.mongodb.repository.ClothRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GoodsServiceImpl implements GoodsService {
+
+    @Autowired
+    ClothRepository clothRepository;
+
+    @Override
+    public GoodsListRes goodsListSearchWord(String keywords) {
+        GoodsListRes goodsListRes = new GoodsListRes();
+
+        List<Cloth> list = clothRepository.findAllByClothNameRegex(keywords + ".*");
+        goodsListRes.setGoodsList(list);
+
+        return goodsListRes;
+    }
+}
