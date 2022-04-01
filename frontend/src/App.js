c5a22da20bf6c47d608540d654a1f398766c0b55
LKH <mogies@naver.com>
2022-04-02 01:40:05 +0900

    chore: resolve conflict
    Former-commit-id: bc631784b2e23fc6b5520fe9dcfd4dd9d9eaee60

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import { Switch, Route, useLocation, useHistory } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'
