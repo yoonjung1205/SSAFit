263b3d588cc887a9b4f7ee344d9f0d2b8261eb98
이혜진 <hy2jinl22@gmail.com>
2022-04-01 22:59:16 +0900

    fix: update recommend female clothes & item detail review loading
    Former-commit-id: 9073f2db26b9be01b8f6a05cd84ae01c5e8137c2

--- frontend/src/pages/itemdetail/components/Reviews.js
+++ frontend/src/pages/itemdetail/components/Reviews.js
@@ -1,11 +1,13 @@
 import CustomAxios from '../../../CustomAxios';
 import React, { useEffect, useState } from 'react';
 import ReviewItem from './ReviewItem';
+import Loading from '../../../components/Loading';
 import '../scss/reviews.scss'
 
 
 const Review = ({ newClothId }) => {
 
+  const [loading, setLoading] = useState(true)
   const [reviews, setReviews] = useState([])
   const [pagenation, setPagenation] = useState({ page: 1, total: 0})
 
@@ -19,10 +21,12 @@ const Review = ({ newClothId }) => {
       setReviews(res.data.items)
       setPagenation({page: res.data.page, total: res.data.total})
     })
+    .then(() => setLoading(false))
     .catch(err => console.log(err, typeof(err)))
   }
 
   useEffect(() => {
+    setLoading(true)
     getReview()
   }, [])
 
@@ -37,10 +41,36 @@ const Review = ({ newClothId }) => {
     }
   }
 
-
-  if (!reviews.length) {
-    return null
+  const ReviewBodies = () => {
+    if (reviews.length) {
+      return (
+        <>
+          <div className='review-body'>
+            {reviews.map(review => (
+              <ReviewItem key={review.reviewId} review={review} no={review.reviewId} />
+            ))}
+          </div>
+          <div className='review-bottom'>
+            
+            <div className='pagenation'>
+              <div onClick={() => {changePage(-1)}}><p>&lt;</p></div>
+              {[1, 2, 3, 4, 5].map(num => (
+                <div className={pagenation.page === num ? 'active': ''} key={num} 
+                  onClick={() => setPagenation({...pagenation, page: num})}
+                >
+                  <p>{num}</p>
+                </div>
+              ))}
+              <div onClick={() => {changePage(1)}}><p>&gt;</p></div>
+            </div>
+  
+          </div>
+        </>
+      )
+    }
+    return <p style={{textAlign: 'center', marginTop: '2rem'}}>리뷰가 없어요...(*￣０￣)ノ</p>
   }
+
 
   return (
     <section className='review-container'>
@@ -48,26 +78,11 @@ const Review = ({ newClothId }) => {
         <h3>Review</h3>
         <p>사용자와 유사한 관심도,신체 사이즈 유저의 리뷰를 제공합니다.</p>
       </div>
-      <div className='review-body'>
-        {reviews.map(review => (
-          <ReviewItem key={review.reviewId} review={review} no={review.reviewId} />
-        ))}
-      </div>
-      <div className='review-bottom'>
-        
-        <div className='pagenation'>
-          <div onClick={() => {changePage(-1)}}><p>&lt;</p></div>
-          {[1, 2, 3, 4, 5].map(num => (
-            <div className={pagenation.page === num ? 'active': ''} key={num} 
-              onClick={() => setPagenation({...pagenation, page: num})}
-            >
-              <p>{num}</p>
-            </div>
-          ))}
-          <div onClick={() => {changePage(1)}}><p>&gt;</p></div>
-        </div>
-
-      </div>
+      {loading ? 
+      <Loading />
+      :
+      <ReviewBodies />
+      }
     </section>
   );
 };
