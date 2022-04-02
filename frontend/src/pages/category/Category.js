37a9ee3724dabe3908a1f1aa02f632a811acf306
이혜진 <hy2jinl22@gmail.com>
2022-04-03 02:10:07 +0900

    fix: update price comma
    Former-commit-id: 27e667c26d1c03af9f66c04b67c5a6aa1f704b71

--- frontend/src/pages/category/Category.js
+++ frontend/src/pages/category/Category.js
@@ -2,34 +2,25 @@ import './scss/Category.scss'
 import NavigationBar from '../../components/NavigationBar'
 import Footer from '../../components/Footer'
 import { Card, Col, Row } from 'react-bootstrap';
-import { cateClothes } from './data';
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Category = () => {
   const params = useParams()
   const path = params.category
-  const [clothes, setClothes] = useState([])
-  const [page, setPage] = useState(1)
+  const clothes = JSON.parse(window.localStorage.getItem('clothes'))
 
-  function changePage (num) {
-    let newPage = page + num
-    if (newPage < 1) {
-      alert('첫번째 페이지 입니다.')
-    } else if (newPage > 5) {
-      alert('마지막 페이지 입니다.')
-    } else {
-      setPage(newPage)
+  const comma = function(tar){
+    let result = ''
+    if (tar){
+      for (let i = tar.length - 1; i >= 0; i--){
+        if (i !== tar.length - 1 && (tar.length - i - 1) % 3 === 0){
+          result = ',' + result
+        }
+        result = tar[i] + result
+      }
     }
+    return result
   }
-
-  useEffect(() => {
-    setClothes(cateClothes)
-  }, [])
-
-  useEffect(() => {
-    console.log('change pagenation', page)
-  }, [page])
 
   return (
     <>
@@ -48,20 +39,11 @@ const Category = () => {
                 <Card.Img src={cloth.clothImg} alt='cloth' />
                 <p className='cate-card-text one-line'>{cloth.clothBrand}</p>
                 <p className='cate-card-text two-line'>{cloth.clothName}</p>
-                <p className='cate-card-text one-line last'>{cloth.clothPrice}원</p>
+                <p className='cate-card-text one-line last'>{comma(String(cloth.clothPrice))}원</p>
               </div>
             </Col>
           ))}
           </Row>
-        </section>
-        <section className='cate-bottom'>
-          <div className='pagenation'>
-            <div onClick={() => {changePage(-1)}}><p>&lt;</p></div>
-            {[1, 2, 3, 4, 5].map(num => (
-              <div className={page === num ? 'active': ''} key={num} onClick={() => setPage(num)}><p>{num}</p></div>
-            ))}
-            <div onClick={() => {changePage(1)}}><p>&gt;</p></div>
-          </div>
         </section>
       </div>
       <Footer />
